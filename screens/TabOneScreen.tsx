d7bcf539ce21094999f9daab4f07fb4552b0ff17
SerialDesign <serialhd@gmail.com>
2023-01-30 11:21:42 +0100

    Query Books

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,13 +1,60 @@
-import { StyleSheet } from "react-native";
+import { ActivityIndicator, StyleSheet } from "react-native";
 
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
+import { gql, useQuery } from "@apollo/client";
+
+const query = gql`
+  query SearchBooks($q: String) {
+    googleBooksSearch(q: $q, country: "US") {
+      items {
+        id
+        volumeInfo {
+          authors
+          averageRating
+          description
+          imageLinks {
+            thumbnail
+          }
+          title
+          subtitle
+          industryIdentifiers {
+            identifier
+            type
+          }
+        }
+      }
+    }
+    openLibrarySearch(q: $q) {
+      docs {
+        author_name
+        title
+        cover_edition_key
+        isbn
+      }
+    }
+  }
+`;
 
 export default function TabOneScreen() {
+  const { data, loading, error } = useQuery(query, {
+    variables: { q: "React Native" },
+  });
+
+  console.log(data);
+  console.log(loading);
+  console.log(error);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
+      {loading && <ActivityIndicator />}
+      {error && (
+        <View>
+          <Text>Error fetching Books</Text>
+          <Text>{error.message}</Text>
+        </View>
+      )}
     </View>
   );
 }
