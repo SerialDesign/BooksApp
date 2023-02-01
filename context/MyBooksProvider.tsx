b39c2f75cf6faf5e1240d6310daa67861e79f067
SerialDesign <serialhd@gmail.com>
2023-02-01 12:50:43 +0100

    My Books Context and Rendering on Tab two

--- context/MyBooksProvider.tsx
+++ context/MyBooksProvider.tsx
@@ -1,16 +1,68 @@
-import { createContext, useContext } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 
+// properly typescript rules
+type MyBooksContextType = {
+  onToggleSave: (book: Book) => void; // fucntion receiving a book and returns void
+  isBookSaved: (book: Book) => boolean; // fucntion receiving a book and returns boolean
+  savedBooks: Book[]; // simply an array of books
+};
+
+// define a context
 // default value for Context
-export const MyBooksContext = createContext({});
+const MyBooksContext = createContext<MyBooksContextType>({
+  //defining default values for type (see above)
+  onToggleSave: () => {}, // = empty function, by default not doing anything when they are initialized, later on in the component the have a proper value
+  isBookSaved: () => false,
+  savedBooks: [],
+});
 
-const MyBooksProvider = ({ children }) => {
+// optional, because of typescript.. othersiwe param in MyBooksProvider w/ red underline, but still works
+type Props = {
+  children: ReactNode;
+};
+
+const MyBooksProvider = ({ children }: Props) => {
+  const [savedBooks, setSavedBooks] = useState<Book[]>([]);
+
+  const areBooksTheSame = (a: Book, b: Book) => {
+    return JSON.stringify(a) === JSON.stringify(b);
+  };
+
+  const isBookSaved = (book: Book) => {
+    // checks if book is already saved - some() expects a comparator function, when at least one of this items fullfilles this condition
+    return savedBooks.some(
+      (savedBook) => areBooksTheSame(savedBook, book) //JSON.stringify() is needed otherwise it only checks by reference. then if a book was checked two times, it has another reference and returns falls. -> with this it compares the string values of the books
+    ); // checks if book (param) is in the savedBooks array
+  };
+
+  // helper function - if setSavedBooks would be passed in value provider it would overwrite everthing, with this method it adds a book to existing list
+  // replaced with toogle Save below
+  //   const addBook = (book: Book) => {
+  //     setSavedBooks((books) => [book, ...books]);
+  //   };
+
+  const onToggleSave = (book: Book) => {
+    if (isBookSaved(book)) {
+      //remove from saved
+      setSavedBooks((books) =>
+        books.filter((savedBook) => !areBooksTheSame(savedBook, book))
+      );
+    } else {
+      // add to saved
+      setSavedBooks((books) => [book, ...books]);
+    }
+  };
+
   return (
-    <MyBooksContext.Provider value={{ test: "a" }}>
+    // wrap context provider around screens which need it
+    // exports functions onToggleSave, isBookSaved of value field + data = savedBooks -> to render it on the page
+    <MyBooksContext.Provider value={{ onToggleSave, isBookSaved, savedBooks }}>
       {children}
     </MyBooksContext.Provider>
   );
 };
 
+// custom hook - resolves problem that in every screen the two imports are needed (context from react + MyBooksContext)
 export const useMyBooks = () => useContext(MyBooksContext);
 
 export default MyBooksProvider;
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,19 +1,25 @@
-import { StyleSheet } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
+import BookItem from "../components/BookItem";
 
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
-import { useContext } from "react";
-import { MyBooksContext } from "../context/MyBooksProvider";
+
+import { useMyBooks } from "../context/MyBooksProvider";
 
 export default function TabTwoScreen() {
-  const context = useContext(MyBooksContext);
-  console.log(context);
+  // const { test } = useMyBooks(); // custom context hook instead of useContext
+  // console.log(test);
+  const { savedBooks } = useMyBooks();
 
   return (
     <View style={styles.container}>
       {/* <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screerns/TabTwoScreen.tsx" /> */}
+      <FlatList
+        data={savedBooks}
+        renderItem={({ item }) => <BookItem book={item} />} // can simply pass the item, because it is already parsed. in MyBooksProvider -> const [savedBooks, setSavedBooks] = useState<Book[]>([]);
+      />
     </View>
   );
 }
@@ -21,8 +27,9 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    // alignItems: "center",
+    // justifyContent: "center",
+    padding: 10,
   },
   title: {
     fontSize: 20,
